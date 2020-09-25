41fa9aba7cde82ebcc0bf849c692ba540f43fca8
Kirk Swenson <kswenson@concord.org>
2020-09-25 16:26:53 -0700

    Update API; update tests

--- src/components/plugin-component.tsx
+++ src/components/plugin-component.tsx
@@ -4,6 +4,7 @@ import * as css from "./plugin-component.sass";
 import DataInspector from "./data-inspector";
 import ActionButtons from "./action-buttons";
 import * as PluginAPI from "@concord-consortium/lara-plugin-api";
+import { ISupportedFeatures } from "@concord-consortium/lara-interactive-api";
 
 export interface IProps {
   authoredState: IAuthoredState;
@@ -15,13 +16,14 @@ interface IState {
   claims: PluginAPI.IJwtClaims | Record<string, unknown>;
   classInfo?: PluginAPI.IClassInfo;
   interactiveState?: PluginAPI.IInteractiveState;
+  messages: string[];
 }
 
 export default class PluginComponent extends React.Component<IProps, IState> {
   public state: IState = {
-    // eslint-disable-next-line react/no-unused-state
     token: "",
-    claims: {}
+    claims: {},
+    messages: []
   };
 
   private wrappedEmbeddableDivContainer = React.createRef<HTMLDivElement>();
@@ -29,15 +31,27 @@ export default class PluginComponent extends React.Component<IProps, IState> {
   public componentDidMount() {
     const {context, authoredState} = this.props;
     const {firebaseAppName} = authoredState;
-    this.getRefToWRap();
+    this.getRefToWrap();
     if (context) {
       this.initialize(context, firebaseAppName || "fake-firebase-app");
+    }
+    const embeddable = context?.wrappedEmbeddable;
+    if (embeddable) {
+      embeddable.onInteractiveAvailable(() => {
+        this.setState(state => ({ messages: [ ...state.messages, "interactiveAvailable" ] }));
+      });
+      embeddable.onInteractiveSupportedFeatures((container: HTMLElement, features: ISupportedFeatures) => {
+        this.setState(state => ({ messages: [ ...state.messages, `interactiveSupportedFeatures: ${JSON.stringify(features)}` ] }));
+        if (features.customMessages?.handles?.["*"]) {
+          embeddable.sendCustomMessage({ type: "debug-plugin:hello", content: "hello" });
+        }
+      });
     }
   }
 
   public render() {
     const {context, authoredState} = this.props;
-    const {classInfo, claims, interactiveState} = this.state;
+    const {classInfo, claims, token, interactiveState, messages} = this.state;
     const headerStyle = {fontFamily: "sans-serif"};
     return (
       <div>
@@ -47,29 +61,29 @@ export default class PluginComponent extends React.Component<IProps, IState> {
           <DataInspector data={context} hideKeys={["container"]} label="Context"/>
           <DataInspector data={classInfo} label="Class Info"/>
           <DataInspector data={interactiveState} label="Interactive State"/>
+          <DataInspector data={token} label="JWT Token"/>
           <DataInspector data={claims} label="JWT Claims"/>
           <DataInspector data={authoredState} label="Authored State"/>
+          <DataInspector data={messages} label="Messages"/>
         </div>
         <ActionButtons/>
       </div>
     );
   }
 
-  private getRefToWRap() {
+  private getRefToWrap() {
     const {context} = this.props;
-    const wrappedEmbeddableDiv = context?.wrappedEmbeddable && context.wrappedEmbeddable.container;
-    if (!wrappedEmbeddableDiv) {
-      return;
+    const wrappedEmbeddableDiv = context?.wrappedEmbeddable?.container;
+    if (wrappedEmbeddableDiv) {
+      const containerNode = this.wrappedEmbeddableDivContainer.current;
+      containerNode?.appendChild(wrappedEmbeddableDiv);
     }
-    const containerNode = this.wrappedEmbeddableDivContainer.current;
-    containerNode?.appendChild(wrappedEmbeddableDiv);
   }
 
   private initialize(context: PluginAPI.IPluginRuntimeContext, appName: string) {
     context.getFirebaseJwt(appName).then((jwtResponse: PluginAPI.IJwtResponse) => {
       if (jwtResponse) {
         this.setState({
-          // eslint-disable-next-line react/no-unused-state
           token: jwtResponse.token,
           claims: jwtResponse.claims
         });
